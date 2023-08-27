e88abeb1b2fe0f41b2ac57577675312bc1f996b7
gogi999 <g.petrovic9@gmail.com>
2023-08-27 13:24:24 +0200

    Setup a professional file structure and new way of implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+import Cart from './features/cart/Cart';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
+import Home from './ui/Home';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
   return (
-    <div>
-      Hello Vite!
-    </div>
-  )
+    <RouterProvider router={router} />
+  );
 }
