e13296c14eed7a4f5506e1361efe315404de04d1
gogi999 <g.petrovic9@gmail.com>
2023-08-28 12:41:31 +0200

    Added displaying a loading indicator, handling errors with error elements and started with tailwind css

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import SearchOrder from '../features/order/SearchOrder';
+
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">
         Fast React Pizza Co.
       </Link>
+      <SearchOrder />
       <p>Mary</p>
     </header>
-  )
+  );
 }
