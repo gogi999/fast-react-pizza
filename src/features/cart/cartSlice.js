aaece9c3784d255e51f6c0865a6cabfa73893f16
gogi999 <g.petrovic9@gmail.com>
2023-09-03 12:31:52 +0200

    Continued to implement redux toolkit

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    // cart: [],
-    cart: [
+    cart: [],
+    /*cart: [
         {
             pizzaId: 12,
             name: 'Mediterranean',
@@ -10,7 +10,7 @@ const initialState = {
             unitPrice: 16,
             totalPrice: 32,
         },
-    ]
+    ];*/
 };
 
 const cartSlice = createSlice({
@@ -54,3 +54,14 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) => 
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) => 
+    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => 
+    (state) => state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
