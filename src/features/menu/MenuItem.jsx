a5aad671ed9f3f0c3cd02624935d2ebfda682ab6
gogi999 <g.petrovic9@gmail.com>
2023-09-05 12:53:55 +0200

    Completed cart feature and started with order feature

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -10,6 +10,7 @@ import {
   getCurrentQuantityById,
 } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -47,7 +48,10 @@ function MenuItem({ pizza }) {
             : <p className="text-sm font-medium text-stone-500 uppercase">Sold out</p>
           }
           {isInCart && (
-            <DeleteItem pizzaId={id} />
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity} />
+              <DeleteItem pizzaId={id} />
+            </div>
           )}
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,41 +1,29 @@
+import { useState } from 'react';
+
 import { useSelector } from 'react-redux';
 import {
   Form,
+  redirect,
   useActionData,
   useNavigation,
 } from 'react-router-dom';
 
+import { createOrder } from '../../services/apiRestaurant';
+import store from '../../store';
 import Button from '../../ui/Button';
+import { formatCurrency } from '../../utils/helpers';
+import {
+  clearCart,
+  getCart,
+  getTotalCartPrice,
+} from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
   const username = useSelector((state) => state.user.username);
@@ -44,8 +32,13 @@ function CreateOrder() {
 
   const formErrors = useActionData();
 
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
@@ -94,8 +87,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -108,7 +101,10 @@ function CreateOrder() {
             disabled={isSubmitting}
             type="primary"  
           >
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+            {isSubmitting 
+              ? 'Placing order...' 
+              : `Order now from ${formatCurrency(totalPrice)}`
+            }
           </Button>
         </div>
       </Form>
@@ -123,7 +119,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
 
   const errors = {};
@@ -135,11 +131,12 @@ export async function action({ request }) {
 
   // If everything is okay, create new order and redirect
 
-  // const newOrder = await createOrder(order);
+  const newOrder = await createOrder(order);
 
-  // return redirect(`/order/${newOrder.id}`);
+  // Do NOT overuse
+  store.dispatch(clearCart());
 
-  return null;
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
